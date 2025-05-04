41b958303d7217f401872ba13eb63f28a1319f8d
Cory (Riley) Black <32332593+RileyMeta@users.noreply.github.com>
2025-05-04 18:32:52 -0400

    Create linearsearch.c

---
+++ C/linearsearch.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+    int numbers[] = {1, 10, 20, 50, 100, 500, 1000};
+
+    for (int i = 0, n = sizeof(numbers); i < n; i++) {
+
+        if (numbers[i] == 50) {
+            printf("Found!\n");
+            return 0;
+        }
+        printf("%i\n", i);
+    }
+
+    printf("Not Found!\n");
+    return 1;
+}
