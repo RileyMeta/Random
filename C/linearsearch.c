ce63c2c2af158d29a0e4696ee47127e39a866c8f
Cory (Riley) Black <32332593+RileyMeta@users.noreply.github.com>
2025-05-04 18:42:27 -0400

    Update linearsearch.c
    +- Corrected the mistake in declaration of `n`

--- C/linearsearch.c
+++ C/linearsearch.c
@@ -3,7 +3,7 @@
 int main(int argc, char* argv[]) {
     int numbers[] = {1, 10, 20, 50, 100, 500, 1000};
 
-    for (int i = 0, n = sizeof(numbers); i < n; i++) {
+    for (int i = 0, n = sizeof(numbers) / sizeof(numbers[0]); i < n; i++) {
 
         if (numbers[i] == 50) {
             printf("Found!\n");
